32c54b2ed73e7281013eabcf89f81f97eae6adac
Chanteng <yrnchanteng@gmail.com>
2022-08-11 12:11:55 +0000

    done with getstarted screen

--- Screens/GetStarted.jsx
+++ Screens/GetStarted.jsx
@@ -42,17 +42,41 @@ const DATA = [
 
 const Indicator = ({ scrollX }) => {
   return (
-    <View style={{ position: "absolute", bottom: 100, flexDirection: "row",}}>
+    <View
+      style={{
+        position: "absolute",
+        bottom: 100,
+        left: 180,
+        flexDirection: "row",
+      }}
+    >
       {DATA.map((_, i) => {
+        const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+        const scale = scrollX.interpolate({
+          inputRange,
+          outputRange: [0.8, 1.4, 0.8],
+          extrapolate: "clamp",
+        });
+        const opacity = scrollX.interpolate({
+          inputRange,
+          outputRange: [0.6, 0.9, 0.6],
+          extrapolate: "clamp",
+        });
         return (
-          <View
+          <Animated.View
             key={`indicator-${i}`}
             style={{
               height: 10,
               width: 10,
               borderRadius: 5,
-              backgroundColor: "#333",
+              backgroundColor: "#fff",
+              opacity,
               margin: 10,
+              transform: [
+                {
+                  scale,
+                },
+              ],
             }}
           />
         );
@@ -61,24 +85,62 @@ const Indicator = ({ scrollX }) => {
   );
 };
 
-
-
 const Backdrop = ({ scrollX }) => {
+  const backgroundColor = scrollX.interpolate({
+    inputRange: bgs.map((_, i) => i * width),
+    outputRange: bgs.map((bg) => bg),
+  });
 
-    const backgroundColor = scrollX.interpolate({
-        inputRange: bgs.map((_, i) => i * width),
-        outputRange: bgs.map((bg) => bg),
-    })
+  return (
+    <Animated.View
+      style={[
+        StyleSheet.absoluteFillObject,
+        {
+          backgroundColor,
+        },
+      ]}
+    />
+  );
+};
 
-    return (
-        <Animated.View 
-        style={[ StyleSheet.absoluteFillObject, {
-            backgroundColor,
-        }]}
-        />
-    )
-}
+const Square = ({ scrollX }) => {
+  const YOLO = Animated.modulo(Animated.divide(
+    Animated.modulo(scrollX, width),
+    new Animated.Value(width)
+  ), 1);
 
+  const rotate = YOLO.interpolate ({
+    inputRange: [0, 0.5, 1],
+    outputRange: ["35deg", "0deg", "35deg"]
+  })
+
+  const translateX = YOLO.interpolate ({
+    inputRange: [0, 0.5, 1],
+    outputRange: [0, -height, 0]
+  })
+
+
+  return (
+    <Animated.View
+      style={{
+        width: height,
+        height: height,
+        backgroundColor: "#fff",
+        borderRadius: 86,
+        position: "absolute",
+        top: -height * 0.6, 
+        left: -height * 0.3,
+        transform: [{
+          rotate,
+        },
+      {
+        translateX,
+      }
+    ]
+      }}
+    />
+  );
+};
 
 const GetStarted = () => {
   const scrollX = React.useRef(new Animated.Value(0)).current;
@@ -87,6 +149,7 @@ const GetStarted = () => {
     <View style={{ flex: 1 }}>
       <StatusBar hidden />
       <Backdrop scrollX={scrollX} />
+      <Square scrollX={scrollX} />
       <Animated.FlatList
         data={DATA}
         keyExtractor={(item) => item.key}
@@ -114,11 +177,18 @@ const GetStarted = () => {
               </View>
               <View style={{ flex: 0.3 }}>
                 <Text
-                  style={{ fontWeight: "800", fontSize: 24, marginBottom: 10 }}
+                  style={{
+                    color: "#fff",
+                    fontWeight: "800",
+                    fontSize: 24,
+                    marginBottom: 10,
+                  }}
                 >
                   {item.title}
                 </Text>
-                <Text style={{ fontWeight: "300" }}>{item.description}</Text>
+                <Text style={{ color: "#fff", fontWeight: "300" }}>
+                  {item.description}
+                </Text>
               </View>
             </View>
           );
